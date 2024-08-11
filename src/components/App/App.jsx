ba7e7e85beea128bc2591b25d4e877da565b6f75
Denys Sherstiuk <denissherstjuk@gmail.com>
2024-08-11 17:51:36 +0300

    1st tast

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,53 +1,17 @@
-import css from "./App.module.css";
+import userData from "../../data/userData.json";
+
 import Profile from "../Profile/Profile";
 
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
+const App = () => {
+  return (
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+  );
 };
-
-function Profile({ profile: { avatar, username, tag, stats } }) {
-  return (
-    <div>
-      <div>
-        <img src={avatar} alt={username} />
-        <p>{username}</p>
-        <p>{tag}</p>
-        <p>{location}</p>
-      </div>
-
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
-        </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
-        </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-function App() {
-  console.log();
-  return (
-    <>
-      <Profile profile={userData} />
-    </>
-  );
-}
 
 export default App;
