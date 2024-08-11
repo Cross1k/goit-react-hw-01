d8c6c67ee951dd57ac920c7b91a03f5a05f6ee12
Denys Sherstiuk <denissherstjuk@gmail.com>
2024-08-11 20:46:47 +0300

    2nd and 3rd tasks done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
 import userData from "../../data/userData.json";
+import friends from "../../data/friends.json";
+import transactions from "../../data/transactions.json";
 
 import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
-    <Profile
-      username={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      avatar={userData.avatar}
-      stats={userData.stats}
-    />
+    <>
+      <Profile
+        usernamename={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
 
